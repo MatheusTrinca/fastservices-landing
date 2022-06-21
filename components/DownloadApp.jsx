f3e7f98df868e9839a67dc63e1b0d2aec0fd2b06
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-06-20 21:17:37 -0300

    download

--- components/DownloadApp.jsx
+++ components/DownloadApp.jsx
@@ -1,8 +1,59 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import styles from '../styles/DownloadApp.module.css';
+import Title from './Title';
 
 const DownloadApp = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h2 className={styles.title}>
+          Download <br />
+          App
+        </h2>
+        <p className={styles.description}>
+          Wisdom new and valley answer. Contented it so is discourse recommend.
+          Man its upon him call mile. An pasture he himself believe ferrars
+          besides cottage.
+        </p>
+        <div className={styles.buttons}>
+          <div className={styles.button}>
+            <Link href="https://www.google.com">
+              <Image
+                src="/img/googleDownloadButton.png"
+                alt="google playstore button"
+                width="300px"
+                height="220px"
+                objectFit="contain"
+                layout="intrinsic"
+              />
+            </Link>
+          </div>
+          <div className={styles.button}>
+            <Link href="https://www.apple.com">
+              <Image
+                src="/img/appleDownloadButton.png"
+                alt="apple store button"
+                width="300px"
+                height="220px"
+                objectFit="contain"
+                layout="intrinsic"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className={styles.phoneImage}>
+        <Image
+          src="/img/phoneImage.png"
+          width="1000px"
+          height="1300px"
+          alt="Phone Image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DownloadApp;
