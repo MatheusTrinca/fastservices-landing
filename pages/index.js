8a397a1c4ed1f2d6b38a556c18a228490fca5347
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-07-01 11:51:45 -0300

    font

--- pages/index.js
+++ pages/index.js
@@ -18,12 +18,6 @@ export default function Home() {
           name="description"
           content="Fast Service App. Encontre os melhores profissionais 24hs por dia"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Roboto:ital,wght@0,300;0,400;1,300&display=swap"
-          rel="stylesheet"
-        />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
       <Header />
